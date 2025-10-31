a27682bd3dd11f35a402094a86eee942c4831792
bharat2005 <bharatvdeshm2005@gmail.com>
2025-10-31 10:28:43 +0530

    add Firebase and Navigation dependencies; implement navigation setup in MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -56,4 +58,13 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    //Firebase
+    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.9.5")
 }
---
+++ app/src/main/java/com/bharat/slimi/core/navigation/ScreenRoutes.kt
@@ -0,0 +1,5 @@
+package com.bharat.slimi.core.navigation
+
+object ScreenRoutes {
+
+}
